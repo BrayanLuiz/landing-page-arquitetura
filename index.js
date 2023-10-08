c750aab2758cba96b44fa834aec63eb0253d4b6a
Brayan Luiz Pereira <b.luiz94@outlook.co>
2023-10-08 18:39:52 -0300

    primeiro desafio

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var seta_direita = window.document.getElementById("seta_direita")
 
 var empreendimento = window.document.getElementById("empreendimento")
 var anos = window.document.getElementById("anos")
-var ptojetos = window.document.getElementById("ptojetos")
+var projetos = window.document.getElementById("projetos")
 
 var seta_esquerda = window.document.getElementById("seta_esquerda")
 
 function rolarparadireita() 
 {
 empreendimento.style = "display:none"
-ptojetos.style = "display:flex"
+projetos.style = "display:flex"
 seta_direita.style = "display:none"
 seta_esquerda.style = "display:flex"
 }
@@ -17,7 +17,7 @@ seta_esquerda.style = "display:flex"
 function rolarparaesquerda()
 {
 empreendimento.style = "display: flex"
-ptojetos.style = "display: none"
+projetos.style = "display: none"
 seta_direita.style = "display:flex"
 seta_esquerda.style = "display:none"
 }
